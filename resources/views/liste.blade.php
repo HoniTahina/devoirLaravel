7149d89d0753c5e0603e53f8f6b0ff481f4dce2f
HoniTahina <ricatahina@gmail.com>
2023-01-09 20:20:49 +0000

    update

--- resources/views/liste.blade.php
+++ resources/views/liste.blade.php
@@ -7,7 +7,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
       </head>
   <body>
-    <h1>Liste des étudiants <a class="btn btn-info" href="/">Ajouter un projet </a></h1>
+    <h1>Liste des projets <a class="btn btn-info" href="/">Ajouter un projet </a></h1>
     <table class="table table-dark table-striped">
   <thead>
     <tr>
